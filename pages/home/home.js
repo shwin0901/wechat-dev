db6d2659b2f1d5f3eba549ef667f92c8771c2e6a
xianggui <401815708@qq.com>
2022-07-03 00:03:21 +0800

    feat: 完善首页功能

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,18 +1,68 @@
-// pages/home/home.js
+import request from '../../utils/request'
+
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    bannerList: [],
+    recommendList: [],
+    topList: [],
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad(options) {
+    this.getBannerList()
+    this.getRecommendList()
+    this.getTopList()
+  },
 
+  getBannerList: async function () {
+    const bannerDatalist = await request('/banner', {
+      type: 2
+    })
+    if (bannerDatalist && bannerDatalist.data.code === 200) {
+      this.setData({
+        bannerList: bannerDatalist.data.banners
+      })
+    }
+  },
+
+  getRecommendList: async function () {
+    const recommendListData = await request('/personalized', {
+      limit: 10
+    })
+    if (recommendListData && recommendListData.data.code === 200) {
+      this.setData({
+        recommendList: recommendListData.data.result
+      })
+    }
+  },
+
+  getTopList: async function () {
+    let index = 0;
+    const resultArr = [];
+    while (index < 5) {
+      let topListData = await request('/top/list', {
+        idx: index++
+      });
+      console.log("topListData", topListData)
+      // splice(会修改原数组，可以对指定的数组进行增删改) slice(不会修改原数组)
+      let topListItem = {
+        name: topListData.data.playlist.name,
+        tracks: topListData.data.playlist.tracks.slice(0, 3)
+      };
+      resultArr.push(topListItem);
+      // 不需要等待五次请求全部结束才更新，用户体验较好，但是渲染次数会多一些
+      
+      this.setData({
+        topList: resultArr
+      })
+    }
+    console.log(this.topList)
   },
 
   /**
