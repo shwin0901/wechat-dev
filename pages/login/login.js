161a5a0dd713c7a8d82a21b1fd8f2ea2f6be91b7
xianggui <401815708@qq.com>
2022-07-03 23:43:26 +0800

    feat: 添加登录页面

---
+++ pages/login/login.js
@@ -0,0 +1,135 @@
+import request from '../../utils/request'
+
+Page({
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    phone: '',
+    password: '',
+  },
+
+  handleInput(event) {
+    console.log(event)
+    const type = event.currentTarget.id
+    const dataType = event.currentTarget.dataset.type
+    this.setData({
+      [type]: event.detail.value
+    })
+  },
+
+  async login() {
+    const {
+      phone,
+      password
+    } = this.data
+    const phoneReg = /^1[3|4|5|6|7|8|9]\d{9}$/;
+
+    if (!phone) {
+      wx.showToast({
+        title: "手机号不能为空",
+        icon: "none"
+      })
+      return
+    }
+    if (!phoneReg.test(phone)) {
+      wx.showToast({
+        title: '手机号格式不正确',
+        icon: "none"
+      })
+      return
+    }
+    if (!password) {
+      wx.showToast({
+        title: '密码不能为空',
+        icon: "none"
+      })
+      return
+    }
+
+    wx.switchTab({
+      url: '/pages/user/user',
+    })
+
+    // const result = await request('/login/cellphone', {
+    //   phone,
+    //   password,
+    //   isLogin: true
+    // })
+    // if (result.data.code === 200) {
+
+    // } else if (result.data.code === 400) {
+    //   wx.showToast({
+    //     title: '手机号错误',
+    //     icon: 'none'
+    //   })
+    // } else if (result.data.code === 502) {
+    //   wx.showToast({
+    //     title: '密码错误',
+    //     icon: 'none'
+    //   })
+    // } else {
+    //   wx.showToast({
+    //     title: '登录失败，请重新登录',
+    //     icon: 'none'
+    //   })
+    // }
+
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage() {
+
+  }
+})
