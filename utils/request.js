02888f066f696ddffdb283e7bfd8a93505a84c91
xianggui <401815708@qq.com>
2022-07-05 00:05:52 +0800

    feat: 添加播放页tab功能

--- utils/request.js
+++ utils/request.js
@@ -1,13 +1,20 @@
 const host = "http://127.0.0.1:3000"
 
-export default (url, data = {}, method = "GET") => {
+export default (url, data = {}, method = "GET", isLogin) => {
   return new Promise((resolve, reject) => {
     wx.request({
       url: host + url,
       data,
       method,
+      header: {
+        cookie: wx.getStorageSync('cookies') ? wx.getStorageSync('cookies').find(item => item.indexOf('MUSIC_U') !== -1) : ''
+      },
       success: (res) => {
-        resolve(res)
+        if (data.isLogin) {
+          console.log("res....", res)
+          wx.setStorageSync('cookies', res.cookies)
+        }
+        resolve(res.data)
       },
       fail: (err) => {
         reject(err)
